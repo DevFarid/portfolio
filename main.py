84ce3278bc49fd3512dfb175b0f2a8dc3a14664f
SixEyes <faridkamizi@gmail.com>
2024-01-07 13:55:26 -0500

    feat(main): add projects and rename stuff

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return "<p>Hello World</p>"
+@app.route('/<name>')
+def index(name=None):
+    return render_template('index.html', name=name)
 
 @app.route('/projects')
 def projects():
@@ -17,11 +18,6 @@ def about():
 @app.route('/contacts')
 def contacts():
     return 'The contact page'
-
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
 
 if __name__ == "__main__":
     app.run(debug=True)
