a6ff376a42548c652ca8810bc5055be2ca438607
SixEyes <faridkamizi@gmail.com>
2024-01-07 13:45:32 -0500

    feat(main): add project loader, and project struct

---
+++ projects/project.py
@@ -0,0 +1,25 @@
+import os
+
+class project_loader:
+
+
+
+    def __init__(self):
+        self.projects = []
+        self.load()
+
+    def load(self):
+        dir_path = os.path.dirname(os.path.abspath(__file__))
+        folders = [folder for folder in os.listdir(dir_path) if os.path.isdir(os.path.join(dir_path, folder))]
+        print(folders)
+
+        
+class project:
+
+    def __init__(self, name):
+        self.name = name
+
+
+
+if __name__ == "__main__":
+    x = project_loader()
