05045588e85d4171d0f2c9d428b86d7e8885a896
SixEyes <faridkamizi@gmail.com>
2024-01-09 20:28:52 -0500

    fix(main): remove __pycache__ from appearing as a project folder

--- projects/project.py
+++ projects/project.py
@@ -9,6 +9,9 @@ class project_loader:
     def load(self):
         dir_path = os.path.dirname(os.path.abspath(__file__))
         self.projects = [folder for folder in os.listdir(dir_path) if os.path.isdir(os.path.join(dir_path, folder))]
+
+        # remove pycache folder each time upon running
+        self.projects.remove("__pycache__")
     
     def getProjects(self):
         return self.projects
